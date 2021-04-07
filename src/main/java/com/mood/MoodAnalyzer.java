a8241ff5c502203274684e77450e4bdf37c3717e
ketankotian <krkkotian@gmail.ocm>
2021-04-07 10:39:39 +0530

    Created : Happy and Sad mood

--- src/main/java/com/mood/MoodAnalyzer.java
+++ src/main/java/com/mood/MoodAnalyzer.java
@@ -2,7 +2,7 @@ package com.mood;
 
 public class MoodAnalyzer {
     public String analyseMood(String message) {
-        if (message.contains("SAD"))
+        if (message.contains("SAD") || message.contains("sad"))
             return "SAD";
         else
             return "HAPPY";
--- src/test/java/com/moodtest/MoodAnalyzerTest.java
+++ src/test/java/com/moodtest/MoodAnalyzerTest.java
@@ -6,16 +6,23 @@ import org.junit.Test;
 
 public class MoodAnalyzerTest {
     @Test
-    public void givenMessage_WhenSad_ShouldReturnSad() {
+    public void givenMessage_WhenSAD_ShouldReturnSad() {
         MoodAnalyzer moodAnalyser = new MoodAnalyzer();
-        String mood = moodAnalyser.analyseMood("This is SAD");
+        String mood = moodAnalyser.analyseMood("I am in SAD mood");
         Assert.assertEquals("SAD", mood);
     }
 
     @Test
     public void givenMessage_WhenNotSad_ReturnHappy() {
         MoodAnalyzer moodAnalyzer = new MoodAnalyzer();
-        String mood = moodAnalyzer.analyseMood("This is HAPPY");
+        String mood = moodAnalyzer.analyseMood("Hello world");
+        Assert.assertEquals("HAPPY", mood);
+    }
+
+    @Test
+    public void givenMessage_WhenHappy_ReturnHappy() {
+        MoodAnalyzer moodAnalyzer = new MoodAnalyzer();
+        String mood = moodAnalyzer.analyseMood("I am in HAPPY mood");
         Assert.assertEquals("HAPPY", mood);
     }
 
